2426aaa6cd1aaeff33068a9621b0486db23ef20e
Sardorbek8877 <sardorbeksafarov8844@gmail.com>
2023-05-21 13:53:55 +0200

    create project

---
+++ src/main/java/com/codingbatapi/CodingbatApiApplication.java
@@ -0,0 +1,13 @@
+package com.codingbatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingbatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingbatApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingbatapi/CodingbatApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingbatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingbatApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
